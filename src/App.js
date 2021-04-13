6d50ee67257972e705052f9a82cbf61600c74bd7
Peter <pitiyke@gmail.com>
2021-04-13 13:49:12 +0100

    added removal of localstorage item for user_details

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import { AuthProvider } from "./context/Context";
 import Landing from "./layout/Landing";
 import Login from "./components/auth/Login";
@@ -8,6 +13,7 @@ import Dashboard from "./components/dashboard/Dashboard";
 import CreateProfile from "./components/create-profile/CreateProfile";
 import ProtectedRoutes from "./utility/ProtectedRoutes";
 import Confirmation from "./components/common/Confirmation";
+import VerifyEmail from "./components/common/VerifyEmail";
 
 function App() {
   return (
@@ -19,7 +25,19 @@ function App() {
           <div className="container">
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-            <Route exact path="/confirm-email" component={Confirmation} />
+            <Route
+              exact
+              path="/api/auth/user/verification/verify-account/:userId/:secretCode"
+              component={VerifyEmail}
+            />
+
+            <Switch>
+              <Redirect
+                from="/resendverification/:email/:id"
+                to="/confirm-email"
+              />
+              <Route exact path="/confirm-email" component={Confirmation} />
+            </Switch>
             <Switch>
               <ProtectedRoutes exact path="/dashboard" component={Dashboard} />
             </Switch>
